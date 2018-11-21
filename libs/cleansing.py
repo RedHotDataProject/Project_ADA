8355eab736ebb048fb72f87f38bc05ffb3475558
Maxence Draguet <maxdraguet@hotmail.com>
2018-11-21 22:17:58 +0100

    country id up

--- libs/cleansing.py
+++ libs/cleansing.py
@@ -1,5 +1,6 @@
 import json
-
+import warnings
+warnings.filterwarnings("ignore", 'This pattern has match groups')
 
 class SmartLookup(dict):
     def __missing__(self, key):
@@ -18,3 +19,38 @@ def to_lookup(dict):
 
 
     return lookup
+
+def country_name_filter(name, countries):
+    """
+    Now assign the country name in different steps : first check if the name to analysed called "name" matches one of the country name (in english) then just return that value. If not, Check that it does not match one of the country label (format cca2 by choice and since it's also in the json for the map so should use this one to compare with the map). Then, if no match yet, check if the "name" is CONTAINED in the alias (set of all country names in different languages and official/non-official). Problem: many returns possible. Check if one of these return has a value of 1 in his forced column, then assign that one. If none is forced or more then one is, don't assign and return the original country name. It would be good in the future to find these instances where none is forced so that we can add them to the list.
+   
+   countries has the following structure : ['name', 'cca2', 'alias', 'Forced']
+   """
+
+    country_set_name = countries[countries.name.apply(lambda x: x.lower()  == name.lower())]
+    if(not country_set_name.empty):
+        if(country_set_name.shape[0]==1):
+            return country_set_name.iloc[0,0]
+      
+    country_set_cca2 = countries[countries.cca2.apply(lambda x: str(x).lower()== name.lower())]
+    if(not country_set_cca2.empty):
+        if(country_set_cca2.shape[0]==1):
+            return country_set_cca2.iloc[0,0]
+     
+    country_set = countries[countries['alias'].str.contains(name, case=False)]
+    if(not country_set.empty):
+        if(country_set.shape[0]==1):
+            return country_set.iloc[0,0]
+        
+        else: # trouble: we have more than one match : check if any is forced !
+            sub_country_set = country_set[country_set.Forced ==1]
+            if(not sub_country_set.empty):
+                if(sub_country_set.shape[0]==1):
+                    return sub_country_set.iloc[0,0]
+            else: 
+                return "Unspecified"
+
+            
+    #If we reach here, nothing strictly matched (1-1) (either more than one non-forced thing or more 
+    #      than one forced thing did).           
+    return "Unknown"
