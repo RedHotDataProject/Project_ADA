230ab171bda0cd5a72bf1d59e0d6774eaefa8de3
Nina Tubau Ribera <tnina@umich.edu>
2018-12-13 23:13:09 +0100

    cleaning code with functions

--- libs/visualising.py
+++ libs/visualising.py
@@ -96,6 +96,7 @@ def plot_occurences_on_map(df, column_key, show_distances=False, title=''):
             colorbar = dict(
                 autotick = True,
                 title = 'Counts [log10]'),
+            colorscale='Viridis'
           ) ]
 
     lines = []
@@ -493,3 +494,34 @@ def make_content_stacked_bar(table, label_column, x_column, y_column):
         ax.set_title("Percentage of the five most important categories per nutrition grade [+others]")
     ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.show()
+    
+
+def palm_oil_overtime(df,df_absolute):   
+    data = [go.Bar(x=df.index,
+               y=df.values,
+                  #text=palm_oil_over_time,
+                   text = df_absolute,
+            marker=dict(color='#097B4E'))]
+
+
+    layout = go.Layout(
+        title='Usage of palm oil over time',
+        xaxis=dict(
+            title='time (years)',
+            titlefont=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'
+            )
+        ),
+        yaxis=dict(
+            title='Percentage of products with palm oil',
+            titlefont=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'))
+    )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    iplot(fig, filename='jupyter-basic_bar')
